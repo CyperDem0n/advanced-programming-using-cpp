38911076e358c15ce53af507b973a297069a7796
Moh Al-Werfally <mohamedgiga8@gmail.com>
2021-02-28 18:54:57 +0200

    Add files via upload
    "# MLOUSC_hw1" 
    Printing a Multiple Line String Using a Single cout Statement :
    Homework1 :
    Advanced Programming Using C++
    Dr. Abdullah Ahmed : OMU Uni

---
+++ mosc_hw1/mosc_hw1.cpp
@@ -0,0 +1,17 @@
+
+
+// Date: 26/02/2021
+// Student Name : Mohammed Hussain Esmail
+// SID : 20194052
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Conditions : using a single cout statement.
+    cout<<"Subject\t\tMarks\nMathematics\t90\nComputer\t77\nChemistry\t69\n\n";
+    return 0;
+}
+
